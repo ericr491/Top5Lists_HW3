4758f6ce17090c8b946a1eb1d8f8e9806636aec6
Eric Ruan <eric.ruan.130@gmail.com>
2021-10-16 14:25:38 -0400

    Bug

---
+++ README.md
@@ -0,0 +1,4 @@
+cd '/c/Program Files/MongoDB/Server/5.0/bin'
+./mongod.exe
+
+Link: https://github.com/ericr491/Top5Lists_HW3
--- server/controllers/top5list-controller.js
+++ server/controllers/top5list-controller.js
@@ -78,19 +78,20 @@ console.log("updateTop5List: " + JSON.stringify(body));
     })
 }
 deleteTop5List = async (req, res) => {
-    await Top5List.findOneAndDelete({ _id: req.params.id }, (err, top5List) => {
-        if (err) {
+    await Top5List.findOneAndDelete({ _id: req.params.id })
+        .then((top5List) => {
+            if (!top5List) {
+                return res
+                    .status(404)
+                    .json({ success: false, error: `Top 5 List not found` })
+            }
+    
+            return res.status(200).json({ success: true, data: top5List })
+        })
+        .catch(err => {
+            console.log(err)
             return res.status(400).json({ success: false, error: err })
-        }
-
-        if (!top5List) {
-            return res
-                .status(404)
-                .json({ success: false, error: `Top 5 List not found` })
-        }
-
-        return res.status(200).json({ success: true, data: top5List })
-    }).catch(err => console.log(err))
+        })
 }
 getTop5ListById = async (req, res) => {
     await Top5List.findOne({ _id: req.params.id }, (err, list) => {
